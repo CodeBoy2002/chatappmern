fa7db5e00706869392274a64c276018d2aa186f6
Sumit <marvelsumit2002@gmail.com>
2024-05-28 18:25:46 +0530

    signup controller complete

--- SERVER/controllers/auth.controller.js
+++ SERVER/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/auth.model.js";
+import bcrypt from 'bcryptjs'
+import generateWebTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
     try {
@@ -13,25 +15,35 @@ export const signupUser = async (req, res) => {
             return res.status(400).json({ error: "Username already exist" })
         }
 
+        //Hash Password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
-        
-        await newUser.save()
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+        if(newUser) {
+            // Generate JWT Token here
+            generateWebTokenAndSetCookie(newUser._id, res)
+            await newUser.save()
+    
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({ error: 'Invalid user data' })
+        }
 
     } catch (error) {
         console.log("Error in signup controller", error.message);   
@@ -39,7 +51,7 @@ export const signupUser = async (req, res) => {
     }
 }
 export const loginUser = async (req, res) => {
-    console.log('Login User');
+    res.json({ message: 'Login User' })
 }
 export const logoutUser = async (req, res) => {
     console.log('Logout User');

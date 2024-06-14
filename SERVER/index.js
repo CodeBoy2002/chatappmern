607ae341cef70c5d880b043e5a1667da8f509cbb
Sumit <marvelsumit2002@gmail.com>
2024-06-14 15:40:43 +0530

    Package json client changes

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -12,7 +13,11 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000
 
-dotenv.config()
+
+//This is used while deploying the application.
+const __dirname = path.resolve()
+
+dotenv.config() 
 
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +25,14 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+//During application deploying
+app.use(express.static(path.join(__dirname, "/CLIENT/dist")))
+
+//During application deploying
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "CLIENT", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
